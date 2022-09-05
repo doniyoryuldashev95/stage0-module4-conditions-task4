46e87a5b1f61dff603b846b8bfef145e73f60640
doniyoryuldashev95 <doniyoryuldashev95@gmail.com>
2022-09-05 10:57:02 +0500

    ALl correct

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int b = first;
+        b = b <= second?second:first;
+        System.out.println(b);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int a = (first>second?first:second)<third?third:(first>second?first:second);
+        System.out.println(a);
     }
 }
